4116c84fe9a027c232536d22157906d9e778497f
Luan Nico <luanpotter27@gmail.com>
2018-05-26 20:05:26 -0300

    adding dartdoc

--- lib/comparing.dart
+++ lib/comparing.dart
@@ -1,12 +1,19 @@
+/// Class with helper methods that allow for easier creation of Comparators.
 class Comparing {
+
+  Comparing._();
+
+  /// Reverses a given comparator, that is, change it from ascending to descending or vice-versa.
   static Comparator<T> reverse<T>(Comparator<T> comparator) {
     return (a, b) => -comparator(a, b);
   }
 
+  /// Returns a Comparator that compares objects of type T by mapping then to Comparables.
   static Comparator<T> on<T>(Comparable Function(T t) mapper) {
     return (a, b) => mapper(a).compareTo(mapper(b));
   }
 
+  /// Join several mappers and compare in order (first the first element, then the second, and so on).
   static Comparator<T> join<T>(List<Comparable Function(T t)> mappers) {
     return (a, b) {
       int r = on(mappers.first)(a, b);
