0d565eefee846c91bc14b377f32535c857b86a57
Luan Nico <luanpotter27@gmail.com>
2018-09-02 18:23:54 -0300

    better syntax

--- lib/ordered_set.dart
+++ lib/ordered_set.dart
@@ -99,7 +99,7 @@ class OrderedSet<E> extends IterableMixin<E> implements Iterable<E> {
     bool result = bucket.remove(e);
     if (result) {
       _length--;
-      _backingSet.remove(new List<E>());
+      _backingSet.remove(<E>[]);
     }
     return result;
   }
