cc210f9e6f8e6cfbff81332a1ea3cb4542e109cb
Luan Nico <luanpotter27@gmail.com>
2021-02-23 16:43:10 -0500

    .

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -15,14 +15,14 @@ dart \
 
 # Run the coverage collector to generate the JSON coverage report.
 echo "Collecting coverage..."
-pub global run coverage:collect_coverage \
+pub run coverage:collect_coverage \
   --port=$OBS_PORT \
   --out=var/coverage.json \
   --wait-paused \
   --resume-isolates
 
 echo "Generating LCOV report..."
-pub global run coverage:format_coverage \
+pub run coverage:format_coverage \
   --lcov \
   --in=var/coverage.json \
   --out=var/lcov.info \
