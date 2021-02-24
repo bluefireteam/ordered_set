c7d9d2a8549d29654b37f2a95c8ff10394077ba6
Luan Nico <luanpotter27@gmail.com>
2021-02-23 20:39:26 -0500

    .

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,8 +3,7 @@
 # TODO(luan) make it generic for any number of files
 pub run coverage:format_coverage \
   --lcov \
-  --in=coverage/test/ordered_set_test.dart.vm.json \
-  --in=coverage/test/comparing_test.dart.vm.json \
+  --in=coverage/test/ \
   --out=coverage/lcov.info \
   --packages=.packages \
   --report-on=lib
