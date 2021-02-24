9de1ebd0793c1c218b21dcae5bc721a7519f3f47
Luan Nico <luanpotter27@gmail.com>
2021-02-23 19:31:16 -0500

    .

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,27 +1,5 @@
 #!/bin/bash -xe
 
-pub get
-
-OBS_PORT=9292
-echo "Collecting coverage on port $OBS_PORT..."
-
-# Start tests in one VM.
-echo "Starting tests..."
-dart \
-  --disable-service-auth-codes \
-  --enable-vm-service=$OBS_PORT \
-  --pause-isolates-on-exit \
-  test &
-
-# Run the coverage collector to generate the JSON coverage report.
-echo "Collecting coverage..."
-pub run coverage:collect_coverage \
-  --port=$OBS_PORT \
-  --out=var/coverage.json \
-  --wait-paused \
-  --resume-isolates
-
-echo "Generating LCOV report..."
 pub run coverage:format_coverage \
   --lcov \
   --in=var/coverage.json \
