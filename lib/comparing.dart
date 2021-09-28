ac9a3efdef68da8a5a2977ad122191be6109ba2a
Luan Nico <luanpotter27@gmail.com>
2021-09-28 16:31:03 -0400

    Wrap all files

--- lib/comparing.dart
+++ lib/comparing.dart
@@ -2,18 +2,20 @@
 class Comparing {
   Comparing._();
 
-  /// Reverses a given comparator, that is, change it from ascending to descending or vice-versa.
+  /// Reverses a given comparator, that is, change it from ascending to
+  /// descending or vice-versa.
   static Comparator<T> reverse<T>(Comparator<T> comparator) {
     return (a, b) => -comparator(a, b);
   }
 
-  /// Returns a Comparator that compares objects of type T by mapping then to Comparables.
+  /// Returns a Comparator that compares objects of type T by mapping then to
+  /// Comparables.
   static Comparator<T> on<T>(Comparable Function(T t) mapper) {
     return (a, b) => mapper(a).compareTo(mapper(b));
   }
 
-  /// Use if you already have a custom comparator you wish to use but your list needs some mapping to get to the
-  /// comparator's type.
+  /// Use if you already have a custom comparator you wish to use but your list
+  /// needs some mapping to get to the comparator's type.
   /// I.e., creates a Comparator<V> given a Comparator<T> and a mapper V -> T.
   static Comparator<V> mapping<T, V>(
     Comparator<T> comparator,
@@ -22,7 +24,8 @@ class Comparing {
     return (a, b) => comparator(mapper(a), mapper(b));
   }
 
-  /// Join several mappers and compare in order (first the first element, then the second, and so on).
+  /// Join several mappers and compare in order (first the first element, then
+  /// the second, and so on).
   static Comparator<T> join<T>(List<Comparable Function(T t)> mappers) {
     return (a, b) {
       final r = on(mappers.first)(a, b);
