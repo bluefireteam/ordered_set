0e73c62a70a83c565caf3a0cdb488651cd623656
Luan Nico <luanpotter27@gmail.com>
2024-06-28 22:09:43 -0300

    Fix

--- lib/comparing.dart
+++ lib/comparing.dart
@@ -27,7 +27,8 @@ class Comparing {
   /// Join several mappers and compare in order (first the first element, then
   /// the second, and so on).
   static Comparator<T> join<T>(
-      List<Comparable<Object?> Function(T t)> mappers) {
+      List<Comparable<Object?> Function(T t)> mappers,
+  ) {
     return (a, b) {
       final r = on(mappers.first)(a, b);
       if (r == 0) {
